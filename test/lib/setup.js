8686a60b1281ac4d89198a2a8f6ffafe950727e6
Ingo Fischer <github@fischer-ka.de>
2017-09-01 09:41:35 +0200

    update testing setup.js

--- test/lib/setup.js
+++ test/lib/setup.js
@@ -499,6 +499,7 @@ function startController(isStartAdapter, onObjectChange, onStateChange, callback
         console.error('Controller is already started!');
     } else {
         console.log('startController...');
+        adapterStarted = false;
         var isObjectConnected;
         var isStatesConnected;
 
@@ -586,6 +587,7 @@ function stopAdapter(cb) {
             }, 0);
         }
     } else {
+        adapterStarted = false;
         pid.on('exit', function (code, signal) {
             if (pid) {
                 console.log('child process terminated due to receipt of signal ' + signal);
@@ -682,4 +684,5 @@ if (typeof module !== undefined && module.parent) {
     module.exports.installAdapter   = installAdapter;
     module.exports.appName          = appName;
     module.exports.adapterName      = adapterName;
+    module.exports.adapterStarted   = adapterStarted;
 }
