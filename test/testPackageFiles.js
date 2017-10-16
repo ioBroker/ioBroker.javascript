299cc2291f15e3c0cc21ccfbd47efdd656be2d36
Ingo Fischer <github@fischer-ka.de>
2017-10-16 16:36:13 +0200

    update basic package-file testing

--- test/testPackageFiles.js
+++ test/testPackageFiles.js
@@ -1,5 +1,7 @@
-/* jshint -W097 */// jshint strict:false
-/*jslint node: true */
+/* jshint -W097 */
+/* jshint strict:false */
+/* jslint node: true */
+/* jshint expr: true */
 var expect = require('chai').expect;
 var fs        = require('fs');
 
@@ -25,6 +27,21 @@ describe('Test package.json and io-package.json', function() {
             console.log('WARNING: No news entry for current version exists in io-package.json, no rollback in Admin possible!');
         }
 
+        expect(ioPackage.common.authors).to.exist;
+        if (ioPackage.common.name.indexOf('template') !== 0) {
+            if (Array.isArray(ioPackage.common.authors)) {
+                expect(ioPackage.common.authors.length).to.not.be.equal(0);
+                if (ioPackage.common.authors.length === 1) {
+                    expect(ioPackage.common.authors[0]).to.not.be.equal('my Name <my@email.com>');
+                }
+            }
+            else {
+                expect(ioPackage.common.authors).to.not.be.equal('my Name <my@email.com>');
+            }
+        }
+        else {
+            console.log('Testing for set authors field in io-package skipped because template adapter');
+        }
         done();
     });
 });
