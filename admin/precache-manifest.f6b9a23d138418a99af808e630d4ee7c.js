c76e85cc1047e35662c2482b7a9fb8426a67c14d
GermanBluefox <dogafox@gmail.com>
2019-07-14 17:31:16 +0200

    (bluefox) update built admin

---
+++ admin/precache-manifest.f6b9a23d138418a99af808e630d4ee7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882bf0d0ae81d551d6dc38f69990b5fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0775abd3197a441eb933",
+    "url": "/static/css/2.600dfb7d.chunk.css"
+  },
+  {
+    "revision": "58028e53d0fecae99a5b",
+    "url": "/static/css/main.6018d82a.chunk.css"
+  },
+  {
+    "revision": "0775abd3197a441eb933",
+    "url": "/static/js/2.8cd898cb.chunk.js"
+  },
+  {
+    "revision": "58028e53d0fecae99a5b",
+    "url": "/static/js/main.76b1bd3d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "770c7d5d68ecffcc2a95322cccc7e387",
+    "url": "/static/media/copy-content.770c7d5d.svg"
+  }
+]);
--- admin/service-worker.js
+++ admin/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7574126543fc13678fc6455f3ad21509.js"
+  "/precache-manifest.f6b9a23d138418a99af808e630d4ee7c.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
