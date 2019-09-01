c61b996b4ffcd066e79747f26664cf450881d71d
Ingo Fischer <github@fischer-ka.de>
2019-09-01 23:37:41 +0200

    update testing setup.js

--- test/lib/setup.js
+++ test/lib/setup.js
@@ -164,7 +164,7 @@ function checkIsControllerInstalled(cb, counter) {
     try {
         var f = fs.readFileSync(dataDir + 'objects.json');
         var objects = JSON.parse(f.toString());
-        if (objects['system.adapter.admin.0']) {
+        if (objects['system.certificates']) {
             console.log('checkIsControllerInstalled: installed!');
             setTimeout(function () {
                 if (cb) cb();
