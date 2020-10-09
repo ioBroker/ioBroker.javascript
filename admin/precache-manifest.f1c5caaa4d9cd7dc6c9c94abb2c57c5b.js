c12eb2952d8b54f4c35a866789b81fffbaa9ddd9
GermanBluefox <dogafox@gmail.com>
2020-10-09 13:28:29 +0200

    Wrap sandbox in async function to enable await

---
+++ admin/precache-manifest.f1c5caaa4d9cd7dc6c9c94abb2c57c5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71882c7947cf1d6921f2d3ecf14b9c68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64087bf8632a2d7a4a1d",
+    "url": "/static/css/2.e0be75cc.chunk.css"
+  },
+  {
+    "revision": "53b37bf4dd3c64fc01b3",
+    "url": "/static/css/main.ec63fb86.chunk.css"
+  },
+  {
+    "revision": "64087bf8632a2d7a4a1d",
+    "url": "/static/js/2.dad9a102.chunk.js"
+  },
+  {
+    "revision": "1583bed55b39251c22a7caf96ba1aeac",
+    "url": "/static/js/2.dad9a102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b37bf4dd3c64fc01b3",
+    "url": "/static/js/main.bccb12a9.chunk.js"
+  },
+  {
+    "revision": "578fa69920ac4a551e6d340a37256bed",
+    "url": "/static/js/main.bccb12a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb40a41b8abfdf8d583",
+    "url": "/static/js/runtime-main.36ac425a.js"
+  }
+]);
