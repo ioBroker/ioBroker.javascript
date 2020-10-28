95c70306a9b6e7d4fcda745a4ba2f3063f91dff9
GermanBluefox <dogafox@gmail.com>
2020-10-28 13:18:25 +0100

    Try to fix blockly async problem

--- src/public/google-blockly/own/blocks_procedures.js
+++ src/public/google-blockly/own/blocks_procedures.js
@@ -152,6 +152,72 @@ Blockly.Procedures.flyoutCategory = function(workspace) {
     populateProcedures(tuple[3], 'procedures_callcustomreturn');
     return xmlList;
 };
+
+// ---------------------- patch for async functions ------------------------------
+// taken from javascript/procedures.js
+Blockly.JavaScript['procedures_defreturn'] = function(block) {
+    // Define a procedure with a return value.
+    var funcName = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('NAME'), Blockly.PROCEDURE_CATEGORY_NAME);
+    var xfix1 = '';
+    if (Blockly.JavaScript.STATEMENT_PREFIX) {
+        xfix1 += Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_PREFIX,
+            block);
+    }
+    if (Blockly.JavaScript.STATEMENT_SUFFIX) {
+        xfix1 += Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_SUFFIX,
+            block);
+    }
+    if (xfix1) {
+        xfix1 = Blockly.JavaScript.prefixLines(xfix1, Blockly.JavaScript.INDENT);
+    }
+    var loopTrap = '';
+    if (Blockly.JavaScript.INFINITE_LOOP_TRAP) {
+        loopTrap = Blockly.JavaScript.prefixLines(
+            Blockly.JavaScript.injectId(Blockly.JavaScript.INFINITE_LOOP_TRAP,
+                block), Blockly.JavaScript.INDENT);
+    }
+    var branch = Blockly.JavaScript.statementToCode(block, 'STACK');
+    var returnValue = Blockly.JavaScript.valueToCode(block, 'RETURN',
+        Blockly.JavaScript.ORDER_NONE) || '';
+    var xfix2 = '';
+    if (branch && returnValue) {
+        // After executing the function body, revisit this block for the return.
+        xfix2 = xfix1;
+    }
+    if (returnValue) {
+        returnValue = Blockly.JavaScript.INDENT + 'return ' + returnValue + ';\n';
+    }
+    var args = [];
+    var variables = block.getVars();
+    for (var i = 0; i < variables.length; i++) {
+        args[i] = Blockly.JavaScript.variableDB_.getName(variables[i],
+            Blockly.VARIABLE_CATEGORY_NAME);
+    }
+    var code = 'async function ' + funcName + '(' + args.join(', ') + ') {\n' +
+        xfix1 + loopTrap + branch + xfix2 + returnValue + '}';
+    code = Blockly.JavaScript.scrub_(block, code);
+    // Add % so as not to collide with helper functions in definitions list.
+    Blockly.JavaScript.definitions_['%' + funcName] = code;
+    return null;
+};
+
+Blockly.JavaScript['procedures_callreturn'] = function(block) {
+    // Call a procedure with a return value.
+    var funcName = Blockly.JavaScript.variableDB_.getName(
+        block.getFieldValue('NAME'), Blockly.PROCEDURE_CATEGORY_NAME);
+    var args = [];
+    var variables = block.getVars();
+    for (var i = 0; i < variables.length; i++) {
+        args[i] = Blockly.JavaScript.valueToCode(block, 'ARG' + i,
+            Blockly.JavaScript.ORDER_COMMA) || 'null';
+    }
+    var code = 'await ' + funcName + '(' + args.join(', ') + ')';
+    return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};
+
+Blockly.JavaScript['procedures_defnoreturn'] =
+    Blockly.JavaScript['procedures_defreturn'];
 // ---------------------- custom function with return ------------------------------
 
 Blockly.Blocks['procedures_defcustomreturn'] = {
