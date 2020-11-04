9c8ef51b2d6cb0f6cf833c0a6c827c5c73a42ccc
GermanBluefox <dogafox@gmail.com>
2020-11-04 18:33:21 +0100

    Update to 0.21.2

--- src/public/vs/configure.js
+++ src/public/vs/configure.js
@@ -10,6 +10,7 @@ require.config({ paths: { 'vs': 'vs' }});
 
 // All languages in monaco-editor
 var availableLanguages = ['de', 'en', 'fr', 'es', 'it', 'ja', 'ru', 'ko', 'zh-tw', 'zh-cn'];
+
 // find the best match
 function findLanguage() {
     if (navigator.languages && Array.isArray(navigator.languages)) {
@@ -18,16 +19,20 @@ function findLanguage() {
     var lang = navigator.language || navigator.userLanguage;
     if (typeof lang === 'string') {
         // first try the long version
-        if (availableLanguages.indexOf(lang) > -1) return lang;
+        if (availableLanguages.indexOf(lang) > -1) {
+            return lang;
+        }
         // then the short one
         lang = lang.substr(0, 2);
-        if (availableLanguages.indexOf(lang) > -1) return lang;
+        if (availableLanguages.indexOf(lang) > -1) {
+            return lang;
+        }
     }
 }
 
 var language = findLanguage();
 // if we have a match, configure the editor
-if (language != null && language !== 'en') {
+if (language !== undefined && language !== null && language !== 'en') {
     require.config({
         'vs/nls': {
             availableLanguages: {
