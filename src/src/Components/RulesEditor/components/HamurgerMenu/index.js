6da8ff1a3ad498a4f8c8440a05ec9a6290086b86
GermanBluefox <dogafox@gmail.com>
2021-02-01 21:44:22 +0100

    Move to React 17 Update Editor

--- src/src/Components/RulesEditor/components/HamurgerMenu/index.js
+++ src/src/Components/RulesEditor/components/HamurgerMenu/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import cls from './hamburgerMenu.module.scss';
 
-export default ({boolean})=>{
+const HamburgerMenu = ({boolean}) => {
     return <div className={`${cls.hamburger_menu} ${boolean ? cls.animate : ''}`}/>
 }
+
+export default HamburgerMenu;
