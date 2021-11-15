f448fa469f0923947dd315622c1e60cba9e55aab
Alexander Groß <agross@therightstuff.de>
2021-11-15 20:15:04 +0100

    When mirroring, monitor symlinked files separately from directories
    When a symlink is monitored, changes to the symlink target do not raise events, because the symlink itself remains unchanged.

---
+++ test/testMirror.js
@@ -0,0 +1,87 @@
+const expect = require("chai").expect;
+const os = require("os");
+const path = require("path");
+const fs = require("fs");
+const Mirror = require("../lib/mirror");
+
+describe("Mirror", () => {
+  describe("File system watcher", () => {
+    let mirror = null;
+    let watched = null;
+
+    beforeEach(() => {
+      watched = fs.mkdtempSync(path.join(os.tmpdir(), "mirror-test-watched-"));
+
+      const noop = () => {};
+
+      mirror = new Mirror({
+        diskRoot: watched,
+        adapter: {
+          namespace: "javascript.0",
+          getForeignObject: noop,
+        },
+      });
+    });
+
+    describe("watchFolders", () => {
+      it("notifies about changes to normal files", (done) => {
+        const script = path.join(watched, "script.js");
+        fs.closeSync(fs.openSync(script, "w"));
+
+        mirror.onFileChange = (_event, file) => {
+          expect(file).to.equal(script);
+
+          done();
+        };
+
+        mirror.watchFolders(watched);
+
+        fs.appendFileSync(script, "some code");
+      });
+
+      it("notifies about changes to symlinked files", (done) => {
+        // Script is located in an unwatched directory...
+        const unwatched = fs.mkdtempSync(path.join(os.tmpdir(), "mirror-test-unwatched-"));
+
+        const script = path.join(unwatched, "script.js");
+        fs.closeSync(fs.openSync(script, "w"));
+
+        // ..but symlinked as a file from a watched directory.
+        const symlink = path.join(watched, "symlinked-script.js");
+        fs.symlinkSync(script, symlink);
+
+        mirror.onFileChange = (_event, file) => {
+          expect(file).to.equal(symlink);
+
+          done();
+        };
+
+        mirror.watchFolders(watched);
+
+        fs.appendFileSync(script, "some code");
+      });
+
+      it("notifies about changes to symlinked directories", (done) => {
+        // Script is located in an unwatched directory...
+        const unwatched = fs.mkdtempSync(path.join(os.tmpdir(), "mirror-test-unwatched-"));
+
+        const script = path.join(unwatched, "script.js");
+        fs.closeSync(fs.openSync(script, "w"));
+
+        // ..but symlinked as a directory from a watched directory.
+        const symlink = path.join(watched, "symlinked-directory");
+        fs.symlinkSync(unwatched, symlink, "dir");
+
+        mirror.onFileChange = (_event, file) => {
+          expect(file).to.equal(path.join(symlink, path.basename(script)));
+
+          done();
+        };
+
+        mirror.watchFolders(watched);
+
+        fs.appendFileSync(script, "some code");
+      });
+    });
+  });
+});
