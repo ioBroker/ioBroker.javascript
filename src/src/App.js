0f21310aefb9dd40690d33d172264d4814cd626b
GermanBluefox <dogafox@gmail.com>
2022-03-23 16:05:00 +0100

    Fixed build process for GUI

--- src/src/App.js
+++ src/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/browser';
 import * as SentryIntegrations from '@sentry/integrations';
 import { withStyles } from '@material-ui/core/styles';
