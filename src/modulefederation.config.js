fc5ea4992fca7fdfb1b88d8ec27164418e8c5409
dilesoft <dilesoft@gmail.com>
2022-06-15 10:30:23 +0300

    federation config

--- src/modulefederation.config.js
+++ src/modulefederation.config.js
@@ -1,8 +1,20 @@
+const makeShared = pkgs => {
+    const result = {};
+    pkgs.forEach(
+        packageName => {
+            result[packageName] = {
+                requiredVersion: '*',
+            };
+        },
+    );
+    return result;
+};
+
 module.exports = {
     name: 'iobroker_javascript',
     remotes: {
     },
-    shared: [
+    shared: makeShared([
         'react', 'react-dom', '@mui/material', '@mui/styles', '@mui/icons-material', 'prop-types', '@iobroker/adapter-react-v5', 'react-ace',
-    ],
+    ]),
 };
