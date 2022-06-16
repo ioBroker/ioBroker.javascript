1bd5f39ee34147143481a4e14232e7506d4895f9
dilesoft <dilesoft@gmail.com>
2022-06-16 09:00:26 +0300

    federation config

--- src/modulefederation.config.js
+++ src/modulefederation.config.js
@@ -4,6 +4,7 @@ const makeShared = pkgs => {
         packageName => {
             result[packageName] = {
                 requiredVersion: '*',
+                singleton: true,
             };
         },
     );
