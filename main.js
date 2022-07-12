121de13ecda1be6cfdea5a28fcb144b998d9ade8
Matthias Kleine <github.com9@emailu.de>
2022-07-12 10:07:50 +0200

    Added log subscription count

--- main.js
+++ main.js
@@ -1663,7 +1663,8 @@ function createVM(source, name) {
     if (!debugMode) {
         source += "\n;\nlog('registered ' + __engine.__subscriptions + ' subscription' + (__engine.__subscriptions === 1 ? '' : 's' ) + '," +
             " ' + __engine.__schedules + ' schedule' + (__engine.__schedules === 1 ? '' : 's' ) + '," +
-            " ' + __engine.__subscriptionsMessages + ' message' + (__engine.__subscriptionsMessages === 1 ? '' : 's' ) + " +
+            " ' + __engine.__subscriptionsMessage + ' message' + (__engine.__subscriptionsMessage === 1 ? '' : 's' ) + '," +
+            " ' + __engine.__subscriptionsLog + ' log' + (__engine.__subscriptionsLog === 1 ? '' : 's' ) + " +
             "' and ' + __engine.__subscriptionsFile + ' file subscription' + (__engine.__subscriptionsFile === 1 ? '' : 's' ));\n";
     } else {
         if (source.startsWith('(async () => {\n')) {
