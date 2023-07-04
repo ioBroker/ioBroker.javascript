40a02a7568c914d228869ec0a68e03e1525ef5cd
Matthias Kleine <github.com9@emailu.de>
2023-07-04 09:55:15 +0200

    require with node-prefix

--- test/testPackageFiles.js
+++ test/testPackageFiles.js
@@ -1,11 +1,7 @@
-/* jshint -W097 */
-/* jshint strict:false */
-/* jslint node: true */
-/* jshint expr: true */
 'use strict';
 
 const expect = require('chai').expect;
-const fs     = require('fs');
+const fs     = require('node:fs');
 
 describe('Test package.json and io-package.json', () => {
     it('Test package files', done => {
