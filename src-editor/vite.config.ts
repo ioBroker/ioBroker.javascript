a489f6a52ff10d616e2ea397a415dff60dc4cb56
dilesoft <dilesoft@gmail.com>
2024-12-12 21:02:35 +0300

    module federation

--- src-editor/vite.config.ts
+++ src-editor/vite.config.ts
@@ -3,6 +3,20 @@ import react from '@vitejs/plugin-react';
 import commonjs from 'vite-plugin-commonjs';
 import svgr from 'vite-plugin-svgr';
 import vitetsConfigPaths from 'vite-tsconfig-paths';
+import { federation } from '@module-federation/vite';
+
+const makeShared = pkgs => {
+    const result = {};
+    pkgs.forEach(
+        packageName => {
+            result[packageName] = {
+                requiredVersion: '*',
+                singleton: true,
+            };
+        },
+    );
+    return result;
+};
 
 export default defineConfig({
   plugins: [
@@ -14,6 +28,14 @@ export default defineConfig({
         'src/**/*.svg',
       ],
     }),
+    federation( {
+        name: 'iobroker_javascript',
+        remotes: {
+        },
+        shared: makeShared([
+            'react', 'react-dom', '@mui/material', '@mui/styles', '@mui/icons-material', 'prop-types', '@iobroker/adapter-react-v5', 'react-ace',
+        ]),
+    })
   ],
   server: {
     port: 3000
