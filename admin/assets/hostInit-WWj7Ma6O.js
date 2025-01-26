7698fb06ae888cf916eedc4f3d4ef68f5cb581c3
GermanBluefox <dogafox@gmail.com>
2025-01-26 17:17:14 +0000

    Bump version

---
+++ admin/assets/hostInit-WWj7Ma6O.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["../remoteEntry.js","./iobroker_javascript__mf_v__runtimeInit__mf_v__-BRRsTaiP.js","./virtualExposes-BIHI7g3E.js","./preload-helper-C1FmrZbK.js"])))=>i.map(i=>d[i]);
+import{_ as i}from"./preload-helper-C1FmrZbK.js";const r=i(()=>import("../remoteEntry.js"),__vite__mapDeps([0,1,2,3]),import.meta.url);Promise.resolve(r).then(e=>Promise.resolve(e.__tla).then(e.init).catch(e.init));
