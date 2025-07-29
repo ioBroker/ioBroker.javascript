f68e70eefc159092af7ebe36b745d04d532e3eb1
GermanBluefox <dogafox@gmail.com>
2025-07-29 13:48:59 +0200

    Compiled

--- admin/custom/assets/ConfigCustomJavascriptSet__loadShare__react_mf_2_dom__loadShare__-DYzjQQsF.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:admin/custom/assets/ConfigCustomJavascriptSet__loadShare__react_mf_2_dom__loadShare__-DibxNPKw.js
-import{g as o}from"./_commonjsHelpers-D6-XlEtG.js";import{C as e,i as r}from"./ConfigCustomJavascriptSet__mf_v__runtimeInit__mf_v__-BaLGCXMi.js";const{loadShare:a}=r,{initPromise:s}=e,_=s.then(t=>a("react-dom",{customShareInfo:{shareConfig:{singleton:!0,strictVersion:!1,requiredVersion:"*"}}})),i=await _.then(t=>t());var n=i;const f=o(n);export{n as C,f as R};
-========
-import{g as o}from"./_commonjsHelpers-D6-XlEtG.js";import{C as e,a as r}from"./ConfigCustomJavascriptSet__mf_v__runtimeInit__mf_v__-Cib3-eWa.js";const{loadShare:a}=r,{initPromise:s}=e,_=s.then(t=>a("react-dom",{customShareInfo:{shareConfig:{singleton:!0,strictVersion:!1,requiredVersion:"*"}}})),i=await _.then(t=>t());var n=i;const f=o(n);export{n as C,f as R};
->>>>>>>> master:admin/custom/assets/ConfigCustomJavascriptSet__loadShare__react_mf_2_dom__loadShare__-DYzjQQsF.js
