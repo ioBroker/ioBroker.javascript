b5224f436e9e6013390606eaaffb189623c4a23f
GermanBluefox <dogafox@gmail.com>
2026-06-08 08:24:14 +0200

    Fixing linter

--- admin/remoteEntry.js
+++ admin/remoteEntry.js
@@ -1,1 +1,1 @@
-import"./assets/preload-helper-BDBacUwf.js";import{g as r,i as a}from"./assets/virtual_mf-REMOTE_ENTRY_ID_iobroker_javascript__remoteEntry_js-DiXVqr03.js";export{r as get,a as init};
+import"./assets/preload-helper-BDBacUwf.js";import{g as r,i as a}from"./assets/virtual_mf-REMOTE_ENTRY_ID_iobroker_javascript__remoteEntry_js-CeYV4czE.js";export{r as get,a as init};
