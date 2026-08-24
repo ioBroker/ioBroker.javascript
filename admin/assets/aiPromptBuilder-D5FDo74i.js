6c7e91426cf00b92270984ca4a630d9849039732
GermanBluefox <dogafox@gmail.com>
2026-08-24 21:59:18 +0300

    Build

--- admin/assets/aiPromptBuilder-D5FDo74i.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./AiChatPanel-C_WbKMGX.js";export{e as buildActionPrompt};
--- admin/remoteEntry.js
+++ admin/remoteEntry.js
@@ -1,7 +1,7 @@
-import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__iobroker_javascript__remoteEntry_js-BwSvIKEV.js";export{t as get,e as init};
+import{n as e,t}from"./assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__iobroker_javascript__remoteEntry_js-BO-WKj5V.js";export{t as get,e as init};
 if (typeof document !== 'undefined' && document.head) {
   try {
-    for (const __mfWarmupPath of ["assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__iobroker_javascript__remoteEntry_js-BwSvIKEV.js","assets/virtualExposes-DgGB-18P.js","assets/virtual_mf-exposes___mfe_internal__iobroker_javascript__remoteEntry_js-ChiJXsfH.js","assets/_virtual_mf-localSharedImportMap___mfe_internal__iobroker_javascript__mf_owner__1-DPmwd_G2.js","assets/dist-CaOsN0XW.js","assets/vite-preload-helper-B7qeedMF.js"]) {
+    for (const __mfWarmupPath of ["assets/virtual_mf-REMOTE_ENTRY_ID___mfe_internal__iobroker_javascript__remoteEntry_js-BO-WKj5V.js","assets/virtualExposes-DgGB-18P.js","assets/virtual_mf-exposes___mfe_internal__iobroker_javascript__remoteEntry_js-ChiJXsfH.js","assets/_virtual_mf-localSharedImportMap___mfe_internal__iobroker_javascript__mf_owner__1-BK6m6sZx.js","assets/dist-CaOsN0XW.js","assets/vite-preload-helper-B7qeedMF.js"]) {
       const __mfWarmupLink = document.createElement('link');
       __mfWarmupLink.rel = 'modulepreload';
       __mfWarmupLink.crossOrigin = '';
